8120d52705fad67b50736da0dc5f8563cce865e1
Mohsen Pourmousa <46406958+Moh-Pou@users.noreply.github.com>
2019-02-06 04:05:42 -0500

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,12 +1,9 @@
 """
-This codes uses the digits dataset.
-The digits dataset represents images of handwritten digits. 
+This codes uses the digits dataset which represents images of handwritten digits. 
 Each image is a 32x32 bitmap. To create a numerical dataset representing the images, 
-each image was divided into non-overlapping blocks of 4x4, and the number of set pixels in each block is counted.
-Thus, each image is represented by an 8x8 matrix of integers in the range 0-16. 
-Each sample in the dataset therefore has 64 attributes. 
-There are a total of 1,797 samples in the dataset.
-Therefore, the digits dataset should be loaded into a Pandas dataframe with 1,797 rows and 64 columns.
+each image is divided into non-overlapping blocks of 4x4, and the number of set pixels in each block is counted.
+Thus, each image is represented by an 8x8 matrix of integers in the range 0-16. Thus, each sample in the dataset has 64 attributes. 
+There are a total of 1,797 samples in the dataset. Therefore, the digits dataset should be loaded into a Pandas dataframe with 1,797 rows and 64 columns.
 The code works as follows:
 First, it loads the digits dataset into a Pandas data frame.
 Next, it preprocesses the digits dataset.
@@ -38,6 +35,12 @@ digits=pd.DataFrame(digits)
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import silhouette_score
 plt.figure(figsize=(10,5))
+"""
+The elbow method consists of visualizing the sum of squared distances between samples and their closest cluster center. 
+This is done by creating a scatter plot where the number of clusters used in K-means is provided on the X axis and the sum of squared distances is provided on the Y axis. 
+If there is a clear inflection point, that can be an indicator of the ideal number of clusters.
+"""
+#Create an elbow plot to examine whether there is a clear inflection point.
 def plot_elbow(dataset, Min_Cluster, max_clusters):
     """Plot elbow curve for k-means."""
     inertias = []
@@ -51,13 +54,15 @@ def plot_elbow(dataset, Min_Cluster, max_clusters):
     plt.xlabel("K")
     plt.ylabel("SSD")
     return inertias
+#Run the K-means algorithm on the digits dataset 24 times and create an elbow plot to look for a clear inflection point.
 ssds=plot_elbow(digits,2,25)
 
+#Use silhouette_score function to calculate Silhouette Coefficient (SC).
 def calc_silhouette(dataset, n):
     kmeans = KMeans(n_clusters=n, random_state=126).fit(dataset)
     score = silhouette_score(dataset, kmeans.labels_)
     return score
-
+#Explore dependence of SC on the number of clusters in K-means algorithm. 
 scores = {n: calc_silhouette(digits, n) for n in range(2, 26)}
 plt.subplot(122)
 plt.plot(
@@ -65,10 +70,8 @@ plt.plot(
     list(scores.values())
 )
 
-scs=list(scores.values())
-
 #Cluster the digits dataset using K-means and evaluate using adjusted rand index (ARI).
-#Set the number of clusters to 10.
+#Set the number of clusters to 10 (there are 10 digits).
 KMeans(n_clusters=10, random_state=126)
 #Evaluate clusters using the ARI.
 from sklearn.metrics import adjusted_rand_score
@@ -76,3 +79,4 @@ model=KMeans(n_clusters=10, random_state=126)
 model.fit(digits)
 labels_pred=model.predict(digits)
 score=adjusted_rand_score(labels, labels_pred)
+print(score)
